0c1df21a6e539c66e0e57526b4eaae475877cc41
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-01-15 23:15:38 -0300

    feat: implement scheduled tasks cleanup

--- pytextify/app.py
+++ pytextify/app.py
@@ -8,11 +8,22 @@ app.include_router(textify.router)
 app.include_router(users.router)
 
 
-# TODO: Criar sistema de autenticação
+# TODO: Adicionar sistema de enviar email para confirmar cadastro
+"""
+O envio de email será seguida a seguinte abordagem:
+- Criação de tabela para armazenar os tokens de confirmação
+- Campo 'is_active' na tabela de usuários
+- Tarefa periódica com celery beat para limpar tokens expirados
+- Cadastro irá gerar um token de confirmação e enviar um email com o link
+- Ao clicar no link, o token será verificado e o usuário será ativado
+- Envio de e-mail com uma task do celery
+- Ativação do cadastro irá adicionar créditos ao usuário
+"""
+# TODO: Adicionar campo 'credits' na tabela de usuários
+# TODO: Atualizar campo 'credits' diariamente para reposição com celery beat
 # TODO: Criar sistema de rate limit
 # TODO: Adicionar limite de quantas tasks cada usuário pode solicitar
 # TODO: Adicionar limite de tamanho de imagem
-# TODO: Criar Enums para os status das tarefas
 # TODO: Adicionar testes
 # TODO: Documentação
 # TODO: Logging com Loki
--- pytextify/tasks.py
+++ pytextify/tasks.py
@@ -1,15 +1,28 @@
 import base64
+from datetime import datetime, timedelta
 from io import BytesIO
 
 import easyocr
 from celery import Celery
+from celery.result import AsyncResult
+from celery.schedules import crontab
 from PIL import Image
+from redis import Redis
 
 from pytextify.settings import Settings
 
 settings = Settings()
 
 celery_app = Celery(broker=settings.BROKER_URL, backend=settings.BACKEND_URL)
+
+
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour=0, minute=0),
+        clear_redis.s(),
+        name='clear_redis',
+    )
 
 
 @celery_app.task
@@ -19,3 +32,16 @@ def extract_text_from_image(image_b64: str):
     result = reader.readtext(image)
     extracted_texts = [text for _, text, _ in result]
     return extracted_texts
+
+
+@celery_app.task
+def clear_redis():
+    redis = Redis.from_url(settings.BACKEND_URL)
+    for key in redis.keys('celery-task-meta-*'):
+        task_id = key.decode('utf-8').replace('celery-task-meta-', '')
+        task = AsyncResult(task_id, app=celery_app)
+        if (
+            task.successful()
+            and task.date_done < datetime.now() - timedelta(days=1)
+        ):
+            redis.delete(key)
